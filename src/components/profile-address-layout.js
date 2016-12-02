650dc076663de6d73dd7e6f924a4d18e52a4c0a5
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2016-12-02 18:05:06 -0200

    Preparing cep view.

--- src/components/profile-address-layout.js
+++ src/components/profile-address-layout.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 
-import { View } from "react-native";
+import {
+  Image,
+  Text,
+  View,
+} from "react-native";
+
+import Layout from "./layout";
+
 
 export default class ProfileAddressLayout extends Component {
   render() {
     return (
-      <View/>
+      <View style={{flex: 1, backgroundColor: "teal"}}>
+        <Layout>
+          <Image source={require("../images/Logo.png")} style={{ alignSelf: "center", marginVertical: 5 }}/>
+
+          <Text style={{
+            fontWeight: "bold",
+            fontSize: 22,
+            color: "white",
+            alignSelf: "center",
+            marginBottom: 20}}>Informe seu CEP</Text>
+
+
+        </Layout>
+      </View>
     );
   }
 }
