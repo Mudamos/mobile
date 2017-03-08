5fb5069388194020bfafa4f59937e9c8ae37deb6
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2017-03-08 11:48:26 -0300

    Add new plips layout

--- src/styles/plips-layout.js
+++ src/styles/plips-layout.js
@@ -1,61 +1,105 @@
-import { StyleSheet } from "react-native";
+import {
+  Dimensions,
+  Platform,
+  StyleSheet,
+} from "react-native";
 
-const ROW_HEIGHT = 130;
-const ROW_BORDER_SIZE = 5;
+const { width: windowWidth, height: windowHeight } = Dimensions.get("window");
+
 
 export default StyleSheet.create({
   full: {
     flex: 1,
   },
-  imageOverlay: {
-    flex: 1,
-    flexDirection: "column-reverse",
+  layoutContent: {
+    backgroundColor: "black",
   },
   listView: {
-    backgroundColor: "#f4f4f4",
-    paddingHorizontal: 10,
-    paddingTop: 10,
+    flex: 1,
   },
   navigationBar: {
-    backgroundColor: "#883DE1",
+    backgroundColor: "transparent",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+  navigationBarContainer: {
+    backgroundColor: "transparent",
+    height: 88,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+  navigationBarGradient: {
+    position: "absolute",
+    flex: 1,
+    height: 88,
+    top: 0,
+    left: 0,
+    right: 0,
   },
   noProjects: {
-    color: "#595959",
+    color: "#fff",
     fontFamily: "roboto",
     fontSize: 14,
     textAlign: "center",
   },
   noProjectsHeader: {
     flex: 1,
-    height: 100,
+    height: 333,
     justifyContent: "center",
+    alignItems: "center",
   },
   plipImage: {
-    height: ROW_HEIGHT - ROW_BORDER_SIZE * 2,
+    minWidth: windowWidth,
+
+    // We wan't to render the image as tall as possible, so we don't see its bounds when scrolling
+    height: windowHeight,
+
+    // This is just to proper position the images up high
+    marginBottom: 100,
+  },
+  plipImageGradient: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  plipTitleContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "transparent",
+  },
+  plipTitleInnerContainer: {
+    flex: 1,
+    paddingHorizontal: 15,
+    paddingVertical: 15,
   },
   plipTitle: {
     color: "#fff",
     fontFamily: "pt sans",
-    fontSize: 24,
+    fontSize: 48,
     fontWeight: "bold",
-    textAlign: "center",
+  },
+  plipSubtitle: {
+    color: "#fff",
+    fontSize: 22,
+
+    ...Platform.select({
+      ios: {
+        fontFamily: "roboto-light",
+      },
+      android: {
+        fontFamily: "roboto_light",
+      },
+    }),
   },
   retryContainer: {
     flex: 1,
     justifyContent: "center",
   },
-  separator: {
-    flex: 1,
-    height: 20,
-  },
   tableRow: {
-    height: ROW_HEIGHT,
-    borderRadius: 5,
-    borderColor: "#fff",
-    borderWidth: ROW_BORDER_SIZE,
-  },
-  titleContainer: {
-    padding: 10,
-    backgroundColor: "transparent",
+    marginTop: 0,
   },
 });
