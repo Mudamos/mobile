35d958119c4279d461d730819b261ad88d1b75a5
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2017-05-17 17:53:16 -0300

    Add android MUDFirebase

---
+++ android/app/src/main/java/org/mudamos/petition/firebase/FirebaseModule.java
@@ -0,0 +1,39 @@
+package org.mudamos.petition.firebase;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+/**
+ * Created by guimello on 5/17/17.
+ */
+
+public class FirebaseModule extends ReactContextBaseJavaModule {
+    private Config config;
+
+    public FirebaseModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+
+        config = new Config();
+    }
+
+    public String getName() {
+        return "MUDFirebase";
+    }
+
+    @ReactMethod
+    public void getStringConfig(String name, Promise promise) {
+        config.getString(name, promise);
+    }
+
+    @ReactMethod
+    public void getNumberConfig(String name, Promise promise) {
+        config.getNumber(name, promise);
+    }
+
+    @ReactMethod
+    public void getBooleanConfig(String name, Promise promise) {
+        config.getBoolean(name, promise);
+    }
+}
---
+++ android/app/src/main/java/org/mudamos/petition/firebase/FirebasePackage.java
@@ -0,0 +1,35 @@
+package org.mudamos.petition.firebase;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by guimello on 5/17/17.
+ */
+
+public class FirebasePackage implements ReactPackage {
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new FirebaseModule(reactContext));
+
+        return modules;
+    }
+}
