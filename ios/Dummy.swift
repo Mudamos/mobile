277b64e0fcaffcd0cb7b1516aa98fffb304d8cc0
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2018-01-09 11:18:28 -0200

    First compile action

---
+++ ios/Dummy.swift
@@ -0,0 +1,13 @@
+//
+//  Dummy.swift
+//  MudamosMobile
+//
+//  Created by Guilherme da Silva Mello on 09/01/18.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+import Foundation
+
+class Dummy {
+
+}
