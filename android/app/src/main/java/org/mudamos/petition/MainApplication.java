22d136275250dc0700c840b2fd4671805dfade5a
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2018-03-08 09:55:47 -0300

    Upgrade to 0.38 with iOS warnings

--- android/app/src/main/java/org/mudamos/petition/MainApplication.java
+++ android/app/src/main/java/org/mudamos/petition/MainApplication.java
@@ -26,6 +26,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import org.mudamos.petition.firebase.FirebasePackage;
 import org.mudamos.petition.signer.SignerPackage;
@@ -75,12 +76,13 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
     FacebookSdk.sdkInitialize(getApplicationContext());
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
