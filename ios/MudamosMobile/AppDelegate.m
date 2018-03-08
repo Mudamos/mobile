4a9660393794ff4ea289baeb1603adda9f49d5f3
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2018-03-08 13:50:41 -0300

    Upgrade to 0.40 no start apps

--- ios/MudamosMobile/AppDelegate.m
+++ ios/MudamosMobile/AppDelegate.m
@@ -9,8 +9,8 @@
 
 #import "AppDelegate.h"
 
-#import "RCTBundleURLProvider.h"
-#import "RCTRootView.h"
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
 #import "IQKeyboardManager.h"
