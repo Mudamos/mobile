3159114b3cbeffd7eb03e4fd607f18be11f37e78
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2018-03-13 09:20:27 -0300

    Updating rn-nodeify issue with randombytes

--- shim.js
+++ shim.js
@@ -3,7 +3,7 @@ if (typeof __filename === 'undefined') global.__filename = ''
 if (typeof process === 'undefined') {
   global.process = require('process')
 } else {
-  var bProcess = require('process')
+  const bProcess = require('process')
   for (var p in bProcess) {
     if (!(p in process)) {
       process[p] = bProcess[p]
@@ -15,8 +15,12 @@ process.browser = false
 if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer
 
 // global.location = global.location || { port: 80 }
-var isDev = typeof __DEV__ === 'boolean' && __DEV__
+const isDev = typeof __DEV__ === 'boolean' && __DEV__
 process.env['NODE_ENV'] = isDev ? 'development' : 'production'
 if (typeof localStorage !== 'undefined') {
   localStorage.debug = isDev ? '*' : ''
 }
+
+// If using the crypto shim, uncomment the following line to ensure
+// crypto is loaded first, so it can populate global.crypto
+require('crypto')
