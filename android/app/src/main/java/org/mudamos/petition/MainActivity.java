f3c8834e111379f08c10685e7a1d9fd44a58f279
Tagview <tagview@Tagviews-MacBook-Pro.local>
2018-04-19 10:40:27 -0300

    Handle deep links

--- android/app/src/main/java/org/mudamos/petition/MainActivity.java
+++ android/app/src/main/java/org/mudamos/petition/MainActivity.java
@@ -1,10 +1,21 @@
 package org.mudamos.petition;
 
 import com.facebook.react.ReactActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
+
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends ReactActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
@@ -18,5 +29,34 @@ public class MainActivity extends ReactActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(getIntent() != null && getIntent().getData() != null) {
+            Toast toast = Toast.makeText(getApplicationContext(), getIntent().getData().toString(), Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(getIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<PendingDynamicLinkData>() {
+                    @Override
+                    public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+                        Uri deepLink = null;
+                        if (pendingDynamicLinkData != null) {
+                            deepLink = pendingDynamicLinkData.getLink();
+
+                        }
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "getDynamicLink:onFailure", e);
+                    }
+                });
     }
 }
