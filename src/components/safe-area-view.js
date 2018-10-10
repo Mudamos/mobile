80bd058bfb979664b65da5c2efd7e0c910128c2b
Tagview <tagview@Tagviews-MacBook-Pro.local>
2018-10-10 15:04:52 -0300

    Code review

--- src/components/safe-area-view.js
+++ src/components/safe-area-view.js
@@ -1,14 +1,17 @@
 import PropTypes from "prop-types";
-import React, { Component } from "react";
+import React from "react";
 
 import {
-  Platform,
   SafeAreaView,
   View,
   ViewPropTypes,
 } from "react-native";
 
 import EStyleSheet from "react-native-extended-stylesheet";
+
+import {
+  isIOSVersionBellow11,
+} from "../utils";
 
 const styles = EStyleSheet.create({
   extraPadding: {
@@ -17,34 +20,19 @@ const styles = EStyleSheet.create({
   },
 });
 
-export default class SafeArea extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-    style: ViewPropTypes.style,
-  };
+const SafeArea = ({ children, style }) => isIOSVersionBellow11 ? (
+    <View style={[styles.extraPadding, style]}>
+      {children}
+    </View>
+  ) : (
+    <SafeAreaView style={style}>
+      {children}
+    </SafeAreaView>
+  );
 
-  get isIOSVersionBellow11() {
-    const majorVersionIOS = parseInt(Platform.Version, 10);
-    return Platform.OS === "ios" && majorVersionIOS <= 11;
-  }
-
-  render() {
-    const {
-      style,
-    } = this.props;
-
-    if (this.isIOSVersionBellow11) {
-      return (
-        <View style={[styles.extraPadding, style]}>
-          {this.props.children}
-        </View>
-      );
-    } else {
-      return (
-        <SafeAreaView style={style}>
-          {this.props.children}
-        </SafeAreaView>
-      );
-    }
-  }
+SafeArea.propTypes = {
+  children: PropTypes.node,
+  style: ViewPropTypes.style,
 }
+
+export default SafeArea;
