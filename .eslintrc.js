3bfb3b1d3d0d1d426eb5007f89c3c6fbcca50454
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2020-12-04 17:26:48 -0300

    Configuring prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  extends: "@react-native-community",
+  rules: {
+    "comma-dangle": [
+      "error",
+      "always-multiline"
+    ],
+    "curly": ["error", "multi-line"],
+    "jest/no-disabled-tests": "off",
+    "no-console": "off",
+    "no-shadow": "off",
+    "quotes": [
+      "error",
+      "double",
+      { avoidEscape: true }
+    ],
+    "react/sort-prop-types": [
+      "warn",
+      {
+        "callbacksLast": true
+      }
+    ]
+  }
+};
