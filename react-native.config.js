4d34df70d5c86c62dd451c662d61e06af393dd85
Guilherme da Silva Mello <guilhermesilvamello@gmail.com>
2020-12-04 23:05:09 -0300

    Fix rn-viewpager ios pod warning

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   dependencies: {
-    // Dependency of rn-viewpager
+    "rn-viewpager": {
+      platforms: {
+        ios: null, // Autolink disabled on iOS
+        android: null, // Autolink disabled on android
+      },
+    },
+    // rn-viewpager dependency
     "@react-native-community/viewpager": {
       platforms: {
-        ios: null, // Autolink disable on iOS
+        ios: null, // Autolink disabled on iOS
         android: {}, // Forcing enabling because this is a dependency of a dependency. Just a Hack for now
       },
     },
